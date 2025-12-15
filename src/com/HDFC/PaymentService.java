eb84e4fc89e51fbf10577b8224cf4eccf0ed13b6
LALA <lalkumar550@gmail.com>
2025-12-15 11:36:58 +0530

    first commit

--- src/com/HDFC/PaymentService.java
+++ src/com/HDFC/PaymentService.java
@@ -1,27 +1,27 @@
 package com.HDFC;
 
 public class PaymentService {
-	
-private  Ipayment payment;
+
+	private Ipayment payment;
 
 	public PaymentService(Ipayment payment) {
-		
+
 		this.payment = payment;
 	}
-	
+
 	public void setpayment(Ipayment payment) {
-		this.payment=payment;
-			
+		this.payment = payment;
+
 	}
+
 	public void DOPayment(double billAmt) {
 		boolean status = payment.ProcessPayment(billAmt);
-		
-		if(status) {
+
+		if (status) {
 			System.out.println("printing Recipt..");
-		}else {
+		} else {
 			System.out.println("Payment Decline..");
 		}
 	}
-	
-	
+
 }
